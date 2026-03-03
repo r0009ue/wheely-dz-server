d2da42d924eb905cde62a1a75ff4ae720e18e04e
Ryn <rayanosoustara33@gmail.com>
2026-03-03 17:01:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -333,7 +333,7 @@ app.post("/end-ride", authenticateToken, async (req, res) => {
   });
 });
 
-aconst PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
